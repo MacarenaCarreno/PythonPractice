50ad388019a30eee8b1c3a730492b33f7cbe923a
Macarena Carreno <macarena.carreno@gmail.com>
2019-09-20 16:06:19 -0400

    adding exercises from 13 to 15

---
+++ 1-Parameters_Unpacking_Variables_13.py
@@ -0,0 +1,12 @@
+print(" ")
+print("   ---------   ###### EXERCISE 13 ##########   ---------   ")
+print(" ")
+# argv is the "Argument varialbe"
+
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)

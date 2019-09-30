211205d180b48861c2efd1aea6c7a534fb738f66
Macarena Carreno <macarena.carreno@gmail.com>
2019-09-30 13:09:51 -0400

    Ex28

--- 14-logic.py
+++ 14-logic.py
@@ -12,7 +12,7 @@ print(1 == 1 and True,      '--',  True)  # True
 print(False and 0 != 0,     '--',  False)  # False
 print(True or 1 == 1,         '--',  True)
 print("test" != "testing",  '--',  True)
-print("test" == 1,          '--',  False)
+print("test" == 1,           '--',  False)
 print(not(True and False),   '--',  True)
 print(not(1 == 1 and 0 != 1), '--',  False)
 print(not(10 == 1 or 1000 == 1000), '--',  False)

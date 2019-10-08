09cf06b3caceef0dad00f937ebb7d522b599df91
Macarena Carreno <macarena.carreno@gmail.com>
2019-10-08 12:23:15 -0400

    functions

---
+++ 21-Accessing_Elements_of_List.py
@@ -0,0 +1,14 @@
+print(" ")
+print("   ---------   ###### EXERCISE 34 ##########   ---------   ")
+print(" ")
+# Page 154
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f'Animal at 1:: {animals[1]}')
+print(f'The third animal:: {animals[2]}')
+print(f'The first Animal {animals[0]}')
+print(f'The animal at 3 {animals[3]}')
+print(f'The fifth animal {animals[4]}')
+print(f'The animal at 2 {animals[2]}')
+print(f'The sixt Animal {animals[5]}')
+print(f'The animal at 4 {animals[4]}')
